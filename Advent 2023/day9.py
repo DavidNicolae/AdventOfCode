711fa886e28473ecf6705df82610bec470aa9ced
Naibed-cloud <davidnicolae5858@gmail.com>
2023-12-10 11:53:07 +0200

    Day 9

---
+++ Advent 2023/day9.py
@@ -0,0 +1,22 @@
+from util import get_input
+
+def a_b(b=False):
+    ans = 0
+    input = get_input()
+    for line in input:
+        line = [int(nr) for nr in line.split()]
+        if b:
+            line = line[::-1]
+        last = line[-1]
+        while line and any(e != 0 for e in line):
+            for i in range(1, len(line)):
+                line[i - 1] = line[i] - line[i - 1]
+            line.pop()
+            last += line[-1]
+        ans += last
+
+    return ans
+
+if __name__ == '__main__':
+    print(a_b())
+    print(a_b(True))
